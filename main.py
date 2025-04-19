24edb5dbdc1288bc39dadc6e34d5fdd348557af9
Harrish P T <harrishpt@gmail.com>
2025-04-19 22:32:31 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+def Hello_world():
+    return {"data": "Hello World, FastAPI"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
