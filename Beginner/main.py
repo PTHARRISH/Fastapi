7eaa8efe49a3a1c207dfed5c03fcebc908c32828
Harrish P T <harrishpt@gmail.com>
2025-06-12 22:31:38 +0530

    Fast api pydantic

--- Beginner/main.py
+++ Beginner/main.py
@@ -1,15 +1,34 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"data": "Hello World!"}
 
 
+class userPostIn(BaseModel):
+    body: str
+
+
+class UserPost(userPostIn):
+    id: int
+
+
+post_table = {}
+
+
+@app.post("/user", response_model=UserPost)
+async def create_post(post: userPostIn):
+    data = post.dict()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id}
-
-
